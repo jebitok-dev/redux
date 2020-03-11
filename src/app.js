ff3864b50f30946b81063f547fb643787a97416f
jebitok-dev <sharonkosgei4@gmail.com>
2020-03-11 12:49:11 +0300

    packages

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<h1>This is my boilerplate</h1>, document.getElementById('app'));
