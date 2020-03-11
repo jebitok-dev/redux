15c85a68ecde931c8f1d7b5ec5f7717dac8bd5ef
jebitok-dev <sharonkosgei4@gmail.com>
2020-03-11 17:23:33 +0300

    refactor code

--- src/app.js
+++ src/app.js
@@ -1,47 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import AppRouter from './routers/AppRouter'; 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-        <div> 
-            This is from my dashboard page
-        </div>
-)
 
-const AddExpensePage = () => (
-    <div> 
-        This is from my add expense component
-    </div>
-)
-
-const EditExpensePage = () => (
-    <div> 
-        This is from my edit expense component
-    </div>
-)
-
-const HelpPage = () => (
-    <div> 
-        This is from my help component
-    </div>
-)
-
-const NotFoundPage = () => (
-    <div>
-        404 Not Found
-    </div>
-)
-const routes = (
-    <BrowserRouter>
-        <Route path="/" component={ExpenseDashboardPage} exact={true}/> {/*exact matches*/}
-        <Route path="/create" component={AddExpensePage}/>
-        <Route path="/edit" component={EditExpensePage}/>
-        <Route path="/help" component={HelpPage}/>
-        <Route component={NotFoundPage}/>
-    </BrowserRouter>
-)
-
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
