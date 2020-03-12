b017ff224703f20836e846d8237c7c821cd937da
jebitok-dev <sharonkosgei4@gmail.com>
2020-03-12 14:44:51 +0300

    redux basics

--- src/playground/redux-100.js
+++ src/playground/redux-100.js
@@ -3,15 +3,28 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0}, action) => {
     switch (action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? 
+            action.incrementBy : 1
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             }   
     case 'DECREMENT':
+        const decrementBy = typeof action.decrementBy === 'number' ?
+        action.decrementBy : 1
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             }
-        default:
-            return state;
+    case 'RESET':
+        return {
+            count: 0
+        }
+    case 'SET':
+       // const count = typeof action.count === 'number' ? action.count : 1
+        return{
+            count: action.count
+        }
+            default:
+                return state;
     }
 });
 store.subscribe(() => {
@@ -19,12 +32,27 @@ store.subscribe(() => {
 })
 //Actions -> an object that gets sent to the store
 store.dispatch ({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 });
 store.dispatch ({
     type: 'INCREMENT'
 });
+
+store.dispatch({
+    type: 'RESET'
+});
+
 store.dispatch ({
     type: 'DECREMENT'
 });
 
+store.dispatch ({
+    type: 'DECREMENT',
+    decrementBy: 10
+});
+
+store.dispatch({
+    type: 'SET',
+    count: 150
+})
