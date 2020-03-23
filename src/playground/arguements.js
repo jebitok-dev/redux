12892ba2f3d6735abb446f31b259cb612e3c3031
jebitok-dev <sharonkosgei4@gmail.com>
2020-03-23 12:25:54 +0300

    fn programming

--- src/playground/arguements.js
+++ src/playground/arguements.js
@@ -16,3 +16,37 @@ var h = binary(f)
 
 g(1,2,3,4)
 h(1,2,3,4)
+
+function flip(fn) {
+    return function flipped(arg1, arg2, ...args) {
+    return fn(arg2, arg1, ...args);
+};
+}
+function f(...args) {
+    console.log(args);
+}
+var g = flip(f)
+g(1,2,3,4)
+
+//REVERSE Method
+function reverseArgs(fn) {
+    return function reversed(...args) {
+        return fn (...args.reverse());
+    };
+}
+function f(...args) {
+    console.log(args);
+}
+var g = reverseArgs(f);
+g(1,2,3,4);
+
+function spreadArgs(fn) {
+    return function spread(args) {
+        return fn(...args);
+    }
+}
+function f(x,y,z,w) {
+    console.log(x+y+z+w);
+}
+var g = spreadArgs(f);
+g([1,2,3,4]);
