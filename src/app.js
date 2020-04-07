02acee36e7866d392771099d1ed129944633506c
jebitok-dev <sharonkosgei4@gmail.com>
2020-04-07 12:37:52 +0300

    testing action creators

--- src/app.js
+++ src/app.js
@@ -4,25 +4,60 @@ import { Provider } from 'react-redux'; //Higher order component
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configurestore';
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
-import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
 
-// store.dispatch(addExpense ({ description : "Water Bill", amount: 9500, createdAt: 15000}));
-// store.dispatch(addExpense({ description : "Gas Bill", amount: 6000, createdAt: 27000}));
-// store.dispatch(addExpense({ description : "Rent", amount: 35700, createdAt: 500}));
+store.dispatch(addExpense ({ description : "Water Bill"}));
+store.dispatch(addExpense({ description : "Gas Bill"}));
+store.dispatch(setTextFilter( "gas"));
+store.dispatch(addExpense ({ description : "Water Bill", amount: 9500, createdAt : 15000}));
+store.dispatch(addExpense({ description : "Gas Bill", amount : 6000, createdAt : 27000}));
+store.dispatch(addExpense({ description : "Rent", amount : 35700, createdAt : 500}));
 
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-//console.log(visibleExpenses);
-    
- const jsx = (
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
     <Provider store = {store}>
         <AppRouter/>
     </Provider>
 )
 
+
+
+ReactDOM.render(<AppRouter/>, document.getElementById('app'));
 ReactDOM.render(jsx, document.getElementById('app'));
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux'; //Higher order component
+// import AppRouter from './routers/AppRouter';
+// import configureStore from './store/configurestore';
+// import { addExpense } from './actions/expenses';
+// import { setTextFilter } from './actions/filters';
+// import getVisibleExpenses from './selectors/expenses';
+// import 'normalize.css/normalize.css';
+// import './styles/styles.scss';
+
+// const store = configureStore();
+
+// // store.dispatch(addExpense ({ description : "Water Bill", amount: 9500, createdAt: 15000}));
+// // store.dispatch(addExpense({ description : "Gas Bill", amount: 6000, createdAt: 27000}));
+// // store.dispatch(addExpense({ description : "Rent", amount: 35700, createdAt: 500}));
+
+// // const state = store.getState();
+// // const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// //console.log(visibleExpenses);
+    
+//  const jsx = (
+//     <Provider store = {store}>
+//         <AppRouter/>
+//     </Provider>
+// )
+
+//ReactDOM.render(<AppRouter/>, document.getElementById('app'));
+
+
+// ReactDOM.render(jsx, document.getElementById('app'));
--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,6 +1,15 @@
 const add = (a, b) => a+b;
+const generateGreeting = (name = Anonymous) => {
+    return `Hi ${name}!`;
+}
 
 test('should add two numbers', ()=> {
     const result= add(5,4);
     expect (result).toBe(9);
+   // expect (result).toBe(8);
 });
+
+test('should generate greeting for no name', ()=> {
+    const result = generateGreeting('');
+    expect (result).toBe('Hi !');
+})
