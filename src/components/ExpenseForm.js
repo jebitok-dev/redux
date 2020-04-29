c412c27a1c5134a56ae583b33691b74a78bf772b
jebitok-dev <sharonkosgei4@gmail.com>
2020-04-29 19:03:00 +0300

    webpack production - sourcemap

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,87 +1,76 @@
 import React from "react";
-import moment from "moment";
-import { singleDatePicker, SingleDatePicker } from "react-dates";
+import { connect } from "react-redux";
+import { DateRangePicker } from "react-dates";
+import {
+  setTextFilter,
+  sortByAmount,
+  sortByDate,
+  setStartDate,
+  setEndDate,
+} from "../actions/filters";
 
-
-export default class ExpenseForm extends React.Component  {
-    constructor(props) {
-        super(props)
-        this.state={
-            description: props.expense ? props.expense.description: "",
-            note:   props.expense ? props.expense.note: "",
-        amount: props.expense(100).toString(),
-        createdAt: props.expense ? moment(props.expense.createdAt): moments,
-        calendarFocused: false,
-        error: ''
-        }        
-    };
-    onDescriptionChange = (e) => {
-        const description = e.target.value;
-        this.setState(() => ({description}))
+export class ExpenseListFilters extends React.Component {
+  state = {
+    focusedInput: null,
+  };
+  onDatesChange = ({ startDate, endDate }) => {
+    this.props.setStartDate(startDate);
+    this.props.setEndDate(endDate);
+  };
+  onFocusChange = (focusedInput) => {
+    this.setState(() => ({ focusedInput }));
+  };
+  onTextChange = (e) => {
+    this.props.setTextFilter(e.target.value);
+  };
+  onSortChange = (e) => {
+    if (e.target.value === "date") {
+      this.props.sortByDate();
+    } else if (e.target.value === "amount") {
+      this.props.sortByAmount();
     }
-    onNoteChange = (e) => {
-        const note = e.target.value;
-        this.setState(() => ({note}))
-    }
-    onAmountChange = (e) => {
-        const amount = e.target.value;
-     if (amount.match(/^\d*(\.\d{0,2})?&/)){
-        this.setState(() => ({amount}))
-    }
-    onDateChange= (createdAt) => {
-        this.setState(() => ({createdAt}));
-    }
-    onFocusChange=({ focused }) => {
-        this.setState(() => ({ calendarFocused: focused}));
-    }
-    onSubmit = (e) => {
-        e.prevent.default()
-
-        if(!this.state.description || !this.state.amount) {
-            this.setState(() => ({error: 'Please provide description and amount'}))
-        } else {
-            this.setState(() => ({error: ''}))
-        } this.props.onSubmit({
-            description: this.state.description,
-            amount: parseFloat(this.state.amount, 10) * 100,
-            createdAt: this.state.createdAt.valueOf(),
-            note: this.state.note
-        })
-    }
+  };
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.props.filters.text}
+          onChange={this.onTextChange}
+        />
+        <select value={this.props.filters.sortBy} onChange={this.onSortChange}>
+          <option value="date">Date</option>
+          <option value="amount">Amount</option>
+        </select>
+        <DateRangePicker
+          startDate={this.props.filters.startDate}
+          endDate={this.props.filters.endDate}
+          onDatesChange={this.onDatesChange}
+          focusedInput={this.state.focusedInput}
+          onFocusChange={this.onFocusChange}
+          showClearDates={true}
+          numberOfMonths={1}
+          isOutsideRange={() => false}
+        />
+      </div>
+    );
+  }
 }
 
-    render() {
-        return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form>
-                    <input
-                    type="text"
-                    placeholder="description"
-                    autoFocus
-                    value={this.state.description}
-                    onChange={this.onDescriptionChange}
-                   />
-                   <input
-                   type="text"
-                   placeholder="amount"
-                   value={this.state.amount}
-                   onChange={this.onAmountChange}
-                   />
-                   <SingleDatePicker
-                   date={this.state.createdAt}
-                   onDateChange={this.onFocusChange}
-                   numberOfMonths={1}
-                   isOutsideRange={()=> false}
-                   />
-                   <textarea
-                   placeholder="Add a note for expense(optional) "
-                   value={this.state.note}
-                   onChange={this.onNoteChange}
-                   ></textarea>
-                   <button>Add Expense</button>
-                </form>
-            </div>
-        )
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    filters: state.filters,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setStartDate: (startDate) => dispatch(setStartDate(startDate)),
+    setEndDate: (endDate) => dispatch(setEndDate(endDate)),
+    setTextFilter: (text) => dispatch(setTextFilter(text)),
+    sortByDate: () => dispatch(sortByDate()),
+    sortByAmount: () => dispatch(sortByAmount()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,50 @@
-const path = require('path');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/, //regex scss
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',//show specific line code on bundle.js file 
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true //show other path apart from the dashboard localhost://8080
-  }
+module.exports = (env) => {
+  const isProduction = env === "production";
+  const ExtractCSS = new ExtractTextPlugin("style.css");
+
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: ExtractCSS.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
+          }),
+        },
+      ],
+    },
+    plugins: [ExtractCSS],
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+    },
+  };
 };
+
